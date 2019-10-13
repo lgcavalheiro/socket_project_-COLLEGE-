deb3a2d12a899eeb7610ecf0c05484e6d4490ac9
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2019-10-13 00:25:19 -0300

    added loopable client

--- src/UDP/client.py
+++ src/UDP/client.py
@@ -2,11 +2,18 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-msg = input("Define mesage:")
+end = False
 
-s.sendto(bytes(msg, "utf-8"),(socket.gethostname(), 1234))
+while not end:
+    fac = input("Define factorial:")
+    s.sendto(bytes(fac, "utf-8"),(socket.gethostname(), 1234))
 
-msg, serversocket = s.recvfrom(1234)
+    msg, serversocket = s.recvfrom(1234)
+    print(msg.decode("utf-8"))
 
-print(msg.decode("utf-8"))
-s.close()
+    opt = input("Another factorial? (Y/n)")
+
+    if opt.lower() == "y" or opt == "":
+        pass
+    else:
+        end = True
