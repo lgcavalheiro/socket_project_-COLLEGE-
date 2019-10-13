f1686edfa580888022d9087f58bbe71cafa2d6ba
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2019-10-12 22:30:15 -0300

    added buffer with header size 16 for handling big factorial results

--- src/TCP/client.py
+++ src/TCP/client.py
@@ -1,17 +1,37 @@
 import socket
 
+HEADERSIZE = 16
 end = False
+
 while not end:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((socket.gethostname(), 1234))
 
-    msg = s.recv(1024)
+    msg = s.recv(64)
     msg = msg.decode("utf-8")
 
     fac = input(msg)
     s.send(bytes(fac, "utf-8"))
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
+
+    full_msg = ''
+    new_msg = True
+    end_msg = False
+    msg_len = 0
+    while not end_msg:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msg_len = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msg_len:
+            print(msg.decode("utf-8"))
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+            end_msg = True
 
     if fac == "-1":
         print("Aborting...")
@@ -23,5 +43,8 @@ while not end:
         if opt.lower() == "y" or opt == "":
             pass
         else:
-            s.close()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((socket.gethostname(), 1234))
+            s.send(bytes("-1", "utf-8"))
             end = True
+            s.close()
--- src/TCP/server.py
+++ src/TCP/server.py
@@ -4,23 +4,29 @@ import math
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
+print("Server is up!")
 
+HEADERSIZE = 16
 end = False
+
 while not end:
     clientsocket, address = s.accept()
 
     print("Connection from {} has been made".format(address))
 
-    clientsocket.send(bytes("Define your factorial (type -1 to abort and close server):", "utf-8"))
-    fac = clientsocket.recv(1024)
+    msg = "Define your factorial (type -1 to abort and close server):"
+    print(f'{len(msg):<{HEADERSIZE}}' + msg)
+
+    clientsocket.send(bytes(msg, "utf-8"))
+    fac = clientsocket.recv(32)
     fac = int(fac.decode("utf-8"))
 
     if fac == -1:
         end = True
         print("Server is closing...")
-        clientsocket.send(bytes("Server is closing...", "utf-8"))
         break
     else:
-        fac = str(math.factorial(fac))
-        clientsocket.send(bytes("Factorial is: " + fac, "utf-8"))
-        clientsocket.close()
+        fac = "Factorial is: " + str(math.factorial(fac))
+        fac = f'{len(fac):<{HEADERSIZE}}' + fac
+        print(f'{len(fac):<{HEADERSIZE}}' + fac)
+        clientsocket.send(bytes(fac, "utf-8"))
